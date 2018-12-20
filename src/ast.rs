686cc9ae3c5d77c2823bf9704785722514618229
Sunjay Varma <varma.sunjay@gmail.com>
2018-12-20 13:50:45 -0700

    Identifier and expression parsing

--- src/ast.rs
+++ src/ast.rs
@@ -1,2 +1,12 @@
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Atom<'a>(pub &'a str);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Ident<'a>(pub &'a str);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Expr<'a> {
+    Atom(Atom<'a>),
+    Ident(Ident<'a>),
+    List(Vec<Expr<'a>>),
+}
--- src/parser.rs
+++ src/parser.rs
@@ -4,12 +4,32 @@ use nom_locate::LocatedSpan;
 
 use crate::ast::*;
 
-type Span<'a> = LocatedSpan<CompleteStr<'a>>;
+pub type Span<'a> = LocatedSpan<CompleteStr<'a>>;
 
 named!(atom(Span) -> Atom, map!(
-    preceded!(char!('\''), take_while1!(|c: char| c.is_alpha() || c == '-')),
+    recognize!(tuple!(char!('\''), take_while1!(|ch: char| ch.is_ascii_alphabetic() || ch == '-'))),
     |LocatedSpan {fragment: CompleteStr(s), ..}| Atom(s)
 ));
+
+named!(ident(Span) -> Ident, map!(
+    recognize!(tuple!(
+        take_while1!(|ch: char| ch.is_ascii_alphabetic() || "+-!$%&*/:<=>?~_^".contains(ch)),
+        take_while!(|ch: char| ch.is_ascii_alphanumeric() || ".+-".contains(ch))
+    )),
+    |id| Ident(id.fragment.0)
+));
+
+named!(expr(Span) -> Expr, alt!(
+    atom => { Expr::Atom } |
+    ident => { Expr::Ident } |
+    list => { Expr::List }
+));
+
+named!(list(Span) -> Vec<Expr>, ws!(delimited!(
+    char!('('),
+    many0!(expr),
+    char!(')')
+)));
 
 #[cfg(test)]
 mod tests {
@@ -17,7 +37,8 @@ mod tests {
 
     macro_rules! test_parser {
         ($parser:ident ( $input:expr ) -> ok) => {
-            let input = Span::new(CompleteStr($input));
+            let input = $input.trim();
+            let input = Span::new(CompleteStr(input));
             match $parser(input) {
                 Ok((remaining, _)) => {
                     assert!(remaining.fragment.0.is_empty(),
@@ -27,7 +48,8 @@ mod tests {
             }
         };
         ($parser:ident ( $input:expr ) -> err) => {
-            let input = Span::new(CompleteStr($input));
+            let input = $input.trim();
+            let input = Span::new(CompleteStr(input));
             match $parser(input) {
                 Ok((ref remaining, ref output)) if remaining.fragment.0.is_empty() => {
                     panic!("parse of `{}` succeeded (when it should have failed). Result: {:?}", $input, output);
@@ -36,7 +58,8 @@ mod tests {
             }
         };
         ($parser:ident ( $input:expr ) -> ok, $expected:expr) => {
-            let input = Span::new(CompleteStr($input));
+            let input = $input.trim();
+            let input = Span::new(CompleteStr(input));
             match $parser(input) {
                 Ok((remaining, output)) => {
                     assert!(remaining.fragment.0.is_empty(),
@@ -50,6 +73,7 @@ mod tests {
 
     #[test]
     fn test_atom() {
+        test_parser!(atom("") -> err);
         test_parser!(atom("'") -> err);
         test_parser!(atom("''") -> err);
         test_parser!(atom("''a") -> err);
@@ -57,11 +81,74 @@ mod tests {
         test_parser!(atom("'ad1") -> err);
         test_parser!(atom("'a-d1") -> err);
 
-        test_parser!(atom("'a") -> ok, Atom("a"));
-        test_parser!(atom("'abcdef") -> ok, Atom("abcdef"));
-        test_parser!(atom("'a-very-happy-atom") -> ok, Atom("a-very-happy-atom"));
-        test_parser!(atom("'-") -> ok, Atom("-"));
-        test_parser!(atom("'-a-a-") -> ok, Atom("-a-a-"));
-        test_parser!(atom("'-------") -> ok, Atom("-------"));
+        test_parser!(atom("'a") -> ok, Atom("'a"));
+        test_parser!(atom("'a") -> ok, Atom("'a"));
+        test_parser!(atom("'abcdef") -> ok, Atom("'abcdef"));
+        test_parser!(atom("'a-very-happy-atom") -> ok, Atom("'a-very-happy-atom"));
+        test_parser!(atom("'-") -> ok, Atom("'-"));
+        test_parser!(atom("'-a-a-") -> ok, Atom("'-a-a-"));
+        test_parser!(atom("'-------") -> ok, Atom("'-------"));
+    }
+
+    #[test]
+    fn test_expr_unmatched() {
+        test_parser!(expr("
+            (car
+                (cons 'ratatouille 'baguette)
+        ") -> err);
+        test_parser!(expr("
+            (car
+                cons 'ratatouille 'baguette))
+        ") -> err);
+    }
+
+    #[test]
+    fn test_expr() {
+        test_parser!(expr("
+            (car
+                (cons 'ratatouille 'baguette))
+        ") -> ok, Expr::List(vec![
+            Expr::Ident(Ident("car")),
+            Expr::List(vec![
+                Expr::Ident(Ident("cons")),
+                Expr::Atom(Atom("'ratatouille")),
+                Expr::Atom(Atom("'baguette")),
+            ])
+        ]));
+
+        test_parser!(expr("
+            (car
+                (cdr
+                    (cons 'ratatouille
+                        (cons 'baguette 'olive-oil))))
+        ") -> ok);
+
+        test_parser!(expr("
+            (Pair Atom Atom)
+        ") -> ok);
+
+        test_parser!(expr("
+            (Pair
+                (cdr
+                    (cons Atom 'olive))
+                (car
+                    (cons 'oil Atom)))
+        ") -> ok);
+
+        test_parser!(expr("
+            (+ (add1
+                (add1 zero))
+              (add1 zero))
+        ") -> ok);
+
+        test_parser!(expr("
+            (define one
+                (add1 zero))
+        ") -> ok);
+
+        test_parser!(expr("
+            (claim two
+                Nat)
+        ") -> ok);
     }
 }
